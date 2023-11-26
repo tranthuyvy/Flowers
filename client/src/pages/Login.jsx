4d787faee23138b617c6aef600f91d7b1b9e8081
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-11-26 21:38:22 +0700

    Login and Register page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
   return (
     <div className='auth'>
-      <h1>Login</h1>
+      <h1>LOGIN</h1>
       <form>
-        <input type='text' placeholder='username'/>
-        <input type='password' placeholder='password'/>
+        <input required type='text' placeholder='Username'/>
+        <input required type='password' placeholder='Password'/>
         <button>Login</button>
       </form>
+      <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
     </div>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='auth'>
+      <h1>REGISTER</h1>
+      <form>
+        <input required type='email' placeholder='Email'/>
+        <input required type='text' placeholder='Username'/>
+        <input required type='password' placeholder='Password'/>
+        <button>Register</button>
+      </form>
+      <span>
+          Do you have an account <Link to="/login">Login</Link>
+        </span>
+    </div>
   )
 }
 
