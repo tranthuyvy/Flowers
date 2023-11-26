e18772a9dfdaefc5bed0652b3d1feb47662954e8
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-11-26 22:30:04 +0700

    Footer and Home Page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Logo from "../images/logo.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Made with ♥️ and <b>React.js</b>.
+      </span>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
