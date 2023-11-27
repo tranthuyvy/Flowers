076c9c2563c86be7b68ed9c5e2b78fb10140a25f
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-11-27 16:05:08 +0700

    first commit

---
+++ api/controllers/authController.js
@@ -0,0 +1,15 @@
+import { db } from "../db.js";
+
+export const register = (req, res) => {
+  //CHECK EXISTING USER
+  const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+
+  db.query(q, [req.body.email, req.body.username], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length) return res.status(409).json("User already exists!");
+  });
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
