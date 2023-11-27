b16b669f68bd412d712681e3099cc571ccc12b14
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-11-27 12:38:17 +0700

    Write (Post) Page

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
+
+  const [value, setValue] = useState('');
+
   return (
-    <div>Write</div>
-  )
-}
+    <div className='add'>
+
+      <div className="content">
+        <input type='text' placeholder='Title'/>
+        <div className="editorContainer">
+          <ReactQuill className="editor" theme="snow" value={value} onChange={setValue} />
+        </div>
+      </div>
+
+      <div className="menu">
+        <div className='item'>
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input
+            style={{ display: "none" }}
+            type="file"
+            id="file"
+            name=""
+            // onChange={(e) => setFile(e.target.files[0])}
+          />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Publish</button>
+          </div>
+        </div>
+        <div className='item'>
+          <h1>Category</h1>
+            <div className="cat">
+              <input
+                className='input'
+                type="radio"
+                // checked={cat === "art"}
+                name="cat"
+                value="art"
+                id="art"
+                // onChange={(e) => setCat(e.target.value)}
+              />
+              <label htmlFor="art">Art</label>
+            </div>
+            <div className="cat">
+              <input
+                className='input'
+                type="radio"
+                // checked={cat === "science"}
+                name="cat"
+                value="science"
+                id="science"
+                // onChange={(e) => setCat(e.target.value)}
+              />
+              <label htmlFor="science">Science</label>
+            </div>
+            <div className="cat">
+              <input
+                className='input'
+                type="radio"
+                // checked={cat === "technology"}
+                name="cat"
+                value="technology"
+                id="technology"
+                // onChange={(e) => setCat(e.target.value)}
+              />
+              <label htmlFor="technology">Technology</label>
+            </div>
+            <div className="cat">
+              <input
+                className='input'
+                type="radio"
+                // checked={cat === "cinema"}
+                name="cat"
+                value="cinema"
+                id="cinema"
+                // onChange={(e) => setCat(e.target.value)}
+              />
+              <label htmlFor="cinema">Cinema</label>
+            </div>
+            <div className="cat">
+              <input
+                className='input'
+                type="radio"
+                // checked={cat === "design"}
+                name="cat"
+                value="design"
+                id="design"
+                // onChange={(e) => setCat(e.target.value)}
+              />
+              <label htmlFor="design">Design</label>
+            </div>
+            <div className="cat">
+              <input
+                className='input'
+                type="radio"
+                // checked={cat === "food"}
+                name="cat"
+                value="food"
+                id="food"
+                // onChange={(e) => setCat(e.target.value)}
+              />
+              <label htmlFor="food">Food</label>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Write
